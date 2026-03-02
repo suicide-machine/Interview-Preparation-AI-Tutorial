4a3265977bb227902f06b5358906a0012e5f1416
Cenote <himangshukalita131@gmail.com>
2026-03-02 10:38:24 +0530

    Backend project setup

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
+
+const app = express()
+
+// Middleware to handle CORS
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+)
+
+app.use(express.json())
+
+// Routes
+
+// Server uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
