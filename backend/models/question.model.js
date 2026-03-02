ae5500c3ba5e14a92ac6e9e0a9d50a770f7fdcea
Cenote <himangshukalita131@gmail.com>
2026-03-02 10:56:45 +0530

    Question model

---
+++ backend/models/question.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+const questionSchema = new mongoose.Schema(
+  {
+    session: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Session",
+    },
+
+    question: {
+      type: String,
+    },
+
+    answer: {
+      type: String,
+    },
+
+    note: {
+      type: String,
+    },
+
+    isPinned: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+)
+
+module.exports = mongoose.model("Question", questionSchema)
