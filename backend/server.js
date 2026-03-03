a0048f3a8b3d6522d2322ce31b60b7e2cee1fb38
Cenote <himangshukalita131@gmail.com>
2026-03-03 10:40:28 +0530

    Connected to database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const path = require("path")
+const connectDB = require("./config/db")
 
 const app = express()
 
@@ -13,6 +14,8 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 )
+
+connectDB()
 
 app.use(express.json())
 
