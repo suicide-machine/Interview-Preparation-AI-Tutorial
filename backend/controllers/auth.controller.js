c3e655ca4c044308d10ad19abf92e975c957b676
Cenote <himangshukalita131@gmail.com>
2026-03-03 11:13:19 +0530

    Register User API

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,43 @@
-const registerUser = async (req, res) => {}
+const User = require("../models/user.model")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+// Generate JWT Token
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" })
+}
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, profileImageUrl } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" })
+    }
+
+    const hashedPassword = await bcrypt.hashSync(password, 10)
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      profileImageUrl,
+    })
+
+    // Return user data with JWT
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    })
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message })
+  }
+}
 
 const loginUser = async (req, res) => {}
 
