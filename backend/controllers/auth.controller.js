4c81960565af625e53a6d868974ebb30b7f251d8
Cenote <himangshukalita131@gmail.com>
2026-03-05 08:52:33 +0530

    Get user profile API

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -67,6 +67,18 @@ const loginUser = async (req, res) => {
   }
 }
 
-const getUserProfile = async (req, res) => {}
+const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password")
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" })
+    }
+
+    res.json(user)
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message })
+  }
+}
 
 module.exports = { registerUser, loginUser, getUserProfile }
