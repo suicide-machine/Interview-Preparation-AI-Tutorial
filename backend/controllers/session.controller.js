870b342297684bce7fb9d7c9a59b375cf277a9db
Cenote <himangshukalita131@gmail.com>
2026-03-05 15:18:22 +0530

    Create session API

--- backend/controllers/session.controller.js
+++ backend/controllers/session.controller.js
@@ -1,4 +1,41 @@
-const createSession = async (req, res) => {}
+const Session = require("../models/session.model")
+const Question = require("../models/question.model")
+
+const createSession = async (req, res) => {
+  try {
+    const { role, experience, topicsToFocus, description, questions } = req.body
+
+    const userId = req.user._id
+
+    const session = await Session.create({
+      user: userId,
+      role,
+      experience,
+      topicsToFocus,
+      description,
+    })
+
+    const questionDocs = await Promise.all(
+      questions.map(async (q) => {
+        const question = await Question.create({
+          session: session._id,
+          question: q.question,
+          answer: q.answer,
+        })
+
+        return question._id
+      }),
+    )
+
+    session.questions = questionDocs
+
+    await session.save()
+
+    res.status(201).json({ success: true, session })
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" })
+  }
+}
 
 const getMySessions = async (req, res) => {}
 
