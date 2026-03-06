9d686e0e62bf641cf38a7409a004e7e8fb728a66
Cenote <himangshukalita131@gmail.com>
2026-03-07 01:42:43 +0530

    Add questions to session API

--- backend/controllers/question.controller.js
+++ backend/controllers/question.controller.js
@@ -1,4 +1,42 @@
-const addQuestionsToSession = async (req, res) => {}
+const Session = require("../models/session.model")
+const Question = require("../models/question.model")
+
+const addQuestionsToSession = async (req, res) => {
+  try {
+    const { sessionId, questions } = req.body
+
+    if (!sessionId || !questions || !Array.isArray(questions)) {
+      return res.status(400).json({ message: "Invalid input data" })
+    }
+
+    const session = await Session.findById(sessionId)
+
+    if (!session) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Session not found" })
+    }
+
+    // create new question
+    const createdQuestions = await Question.insertMany(
+      questions.map((q) => ({
+        session: sessionId,
+        question: q.question,
+        answer: q.answer,
+      })),
+    )
+
+    session.questions.push(...createdQuestions.map((q) => q._id))
+
+    await session.save()
+
+    res.status(201).json(createdQuestions)
+  } catch (error) {
+    console.log(error)
+
+    res.status(500).json({ success: false, message: "Server Error" })
+  }
+}
 
 const togglePinQuestion = async (req, res) => {}
 
